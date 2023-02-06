94e2c2a6649745f67854482bf3f50d6005559a62
gdavid2 <124572489+gdavid2@users.noreply.github.com>
2023-02-06 17:55:44 +0100

    premier commit

---
+++ commun/include.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_H_INCLUDED
+#define INCLUDE_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+#endif // INCLUDE_H_INCLUDED
---
+++ commun/print.h
@@ -0,0 +1,6 @@
+#ifndef PRINT_H_INCLUDED
+#define PRINT_H_INCLUDED
+
+
+
+#endif // PRINT_H_INCLUDED
